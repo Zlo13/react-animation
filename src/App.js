7859882154870c4b0796d6bc3043a7dd987c4e0a
Andrey <ognivenkoav@gmail.com>
2020-12-09 22:50:05 +0300

    126 Этапы работы анимаций

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ import { Transition } from 'react-transition-group'
           }}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
         >
           {state => <div className={`square blue ${state}`}>{state}</div>}
         </Transition>
